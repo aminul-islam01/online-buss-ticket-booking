ab6f903bc964b2a38e79d2d7a83e326ce68d7209
aminul-islam01 <abiddut953@gmail.com>
2023-07-17 00:14:22 +0600

    add booking section

--- src/Pages/OurTrip/OurTrip.jsx
+++ src/Pages/OurTrip/OurTrip.jsx
@@ -3,17 +3,24 @@ import { Link } from "react-router-dom";
 
 
 const OurTrip = () => {
-    const [trips, setTrips] = useState([]);
+    const [trips1, setTrips1] = useState([]);
+    const [trips2, setTrips2] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:5000/trips')
         .then(res => res.json())
-        .then(data => setTrips(data))
+        .then(data => {
+            const upTrip = data.filter(result => result.form === 'Dhaka');
+            const downTrip = data.filter(result => result.form === 'Khulna');
+            setTrips1(upTrip)
+            setTrips2(downTrip)
+        })
     }, [])
-
+   
     return (
         <div className="my-20 grid grid-cols-2 gap-5">
             <div className="overflow-x-auto">
+                <h2 className="text-2xl font-semibold text-center mb-5">Dhaka To Khulna</h2>
                 <table className="table">
                     {/* head */}
                     <thead>
@@ -26,7 +33,7 @@ const OurTrip = () => {
                     </thead>
                     <tbody>
                         {
-                            trips.map(trip => 
+                            trips1.map(trip => 
                                 <tr key={trip._id}>
                                 <th>{trip.coachNo}</th>
                                 <td>{trip.bussName}</td>
@@ -44,38 +51,33 @@ const OurTrip = () => {
                 </table>
             </div>
             <div className="overflow-x-auto">
+            <h2 className="text-2xl font-semibold text-center mb-5">Khulna To Dhaka</h2>
                 <table className="table">
                     {/* head */}
                     <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                        <tr className="bg-base-200">
+                            <th>Coach No</th>
+                            <th>Buss Name</th>
+                            <th>Start Time</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr className="bg-base-200">
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
+                        {
+                            trips2.map(trip => 
+                                <tr key={trip._id}>
+                                <th>{trip.coachNo}</th>
+                                <td>{trip.bussName}</td>
+                                <td>{trip.startTime}</td>
+                                <td>
+                                    <Link to= {`/see-ticket/${trip._id}`}>
+                                        <button className="btn btn-primary btn-sm">See Tricket</button>
+                                    </Link>
+                                </td>
+                            </tr>
+                            )
+                        }
+
                     </tbody>
                 </table>
             </div>
--- src/Pages/SeeTicket/SeeTicket.jsx
+++ src/Pages/SeeTicket/SeeTicket.jsx
@@ -5,8 +5,9 @@ import { useForm } from "react-hook-form"
 const SeeTicket = () => {
     const [select, setSelect] = useState(false);
     const [trip, setTrip] = useState({});
+    const [rent, setRent] = useState();
     const seats = useLoaderData();
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, watch } = useForm()
     const id = useParams();
     const { form, coachNo, startTime, date } = trip;
 
@@ -16,8 +17,7 @@ const SeeTicket = () => {
             .then(data => setTrip(data))
     }, [id])
 
-    console.log(trip)
-    console.log(seats)
+    console.log()
 
     const handleSelect = (seat) => {
         setSelect(!select);
@@ -27,6 +27,14 @@ const SeeTicket = () => {
     const onSubmit = (data) => {
         console.log(data)
     }
+
+    const handleTo = () => {
+        const to = watch('to');
+        if (to === 'Dhaka') {
+            setRent(500)
+        }
+    };
+
 
     return (
         <div>
@@ -54,56 +62,61 @@ const SeeTicket = () => {
                 <div className="hero min-h-screen px-5">
                     <form onSubmit={handleSubmit(onSubmit)} className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <h1 className="text-4xl font-bold text-center">Add New Trip</h1>
-                            <div className="grid md:grid-cols-2 gap-5">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Coach No</span>
-                                    </label>
-                                    <input type="number" {...register("coachNo")} className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Buss Name</span>
-                                    </label>
-                                    <input type="text" {...register("bussName")} className="input input-bordered" />
-                                </div>
-                            </div>
                             <div className="grid md:grid-cols-2 gap-5">
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Form:</span>
                                     </label>
-                                    <input type="text" {...register("form")} className="input input-bordered" defaultValue={form}readOnly />
+                                    <select {...register("form")} className="input input-bordered">
+                                        {form === 'Dhaka' ? <option value="Dhaka">Dhaka</option>
+                                            : <option value="Khulna">Khulna</option>}
+                                        <option value="Gopalgonj">Gopalgonj</option>
+                                        <option value="Narail">Narail</option>
+                                        <option value="Pirojpur">Pirojpur</option>
+                                    </select>
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">To:</span>
                                     </label>
-                                    <select {...register("to")} className="input input-bordered">
-                                        <option value="Dhaka">Dhaka</option>
+                                    <select onClick={handleTo} {...register("to")} className="input input-bordered">
+                                        {form === 'Khulna' ? <option value="Dhaka">Dhaka</option>
+                                            : <option value="Khulna">Khulna</option>}
                                         <option value="Gopalgonj">Gopalgonj</option>
                                         <option value="Narail">Narail</option>
                                         <option value="Pirojpur">Pirojpur</option>
-                                        <option value="Khulna">Khulna</option>
                                     </select>
                                 </div>
                             </div>
                             <div className="grid md:grid-cols-2 gap-5">
                                 <div className="form-control">
                                     <label className="label">
-                                        <span className="label-text">Date</span>
+                                        <span className="label-text">Rent Per Seat (TK)</span>
                                     </label>
-                                    <input type="date" {...register("date")} className="input input-bordered" />
+                                    <input type="text" {...register("rent")} className="input input-bordered" defaultValue={rent} />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
-                                        <span className="label-text">Start Time</span>
+                                        <span className="label-text">Total Rent (TK)</span>
                                     </label>
-                                    <input type="time" {...register("startTime")} className="input input-bordered" />
+                                    <input type="text" {...register("total rent")} className="input input-bordered" />
                                 </div>
                             </div>
-                            <button className="btn btn-primary">Add Now</button>
+                            <div className="grid md:grid-cols-2 gap-5">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text" {...register("name")} className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Phone Number</span>
+                                    </label>
+                                    <input type="number" {...register("phoneNumber")} className="input input-bordered" />
+                                </div>
+                            </div>
+                            <button className="btn btn-primary">Confirm</button>
                         </div>
                     </form>
                 </div>
